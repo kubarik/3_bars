21713287e2589e1b191ee131448543cf13898b73
kubarik <webpavlova@gmail.com>
2018-01-10 01:48:51 +0300

    big/small/closest bars

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # Ближайшие бары
 
-[FIXME. Здесь будет описание проекта]
+На основании открытых данных (data.mos.ru), скрипт определяет:
+
+- самый большой бар;
+- самый маленький бар;
+- самый близкий бар (текущие gps-координаты пользователь введет с клавиатуры).
+
+# Как использовать
+
+Сначала получаем десериализацию JSON, с помощью функции:
+```python
+load_data(filepath)
+```
+где
+  filepath - путь до файла с произвольными данными в формате JSON.
+Если по указанному пути нет файла, то попытаемся его забрать с URLа
+```python
+url_json = 'https://apidata.mos.ru/v1/features/1796?api_key=c9d98de8f9a903176268131e2a9821d4'
+barsjson = urlopen(url_json)
+```
+
+В случае успеха, выполняется функции и получаем результат:
+```python
+print ("самый большой бар: ", get_biggest_bar(deserializes_json))
+print ("самый маленький бар: ", get_smallest_bar(deserializes_json))
+print("самый близкий бар: ", get_closest_bar(deserializes_json, longitude, latitude))
+```
+где
+  deserializes_json - JSON, загруженный из файла.
+  longitude и latitude - gps-координаты, введенные пользователем с клавиатуры
+
+Импортируемые модули
+```python
+import json #кодирование и декодирование данных
+import os #для работы с ОС
+import re #работа с регулярными выражениями
+from urllib.request import urlopen #работа с HTTP
+from math import sqrt
+```
 
 # Как запустить
 
@@ -10,9 +47,13 @@
 
 ```bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
-# FIXME вывести пример ответа скрипта
-
+$ python bars.py
+# Пример ответа
+самый большой бар:  Спорт бар «Красная машина»
+самый маленький бар:  Сушистор
+Enter longitude:33
+Enter latitude:32.1
+самый близкий бар:  Корпорация Бар
 ```
 
 Запуск на Windows происходит аналогично.
--- bars.py
+++ bars.py
@@ -1,21 +1,60 @@
 import json
-
+import os
+import re
+from urllib.request import urlopen
+from math import sqrt
 
 def load_data(filepath):
-    pass
-
+    if (os.path.isfile(filepath) == False):
+        url_json = 'https://apidata.mos.ru/v1/features/1796?api_key=c9d98de8f9a903176268131e2a9821d4'
+        barsjson = urlopen(url_json)
+        response = json.loads(barsjson.read().decode('utf-8'))
+        with open(filepath, "w") as filesave:
+            json.dump(response, filesave)
+        return response
+    else:
+        return json.load(open(filepath))
 
 def get_biggest_bar(data):
-    pass
+    bar = max(data['features'], key = lambda x: x['properties']['Attributes']['SeatsCount'])
 
+    return bar['properties']['Attributes']['Name'] if bar else "failed"
 
 def get_smallest_bar(data):
-    pass
+    bar = min(data['features'], key = lambda x: x['properties']['Attributes']['SeatsCount'])
 
+    return bar['properties']['Attributes']['Name'] if bar else "failed"
 
 def get_closest_bar(data, longitude, latitude):
-    pass
 
+    reg_exp = '^-?[0-9]{1,3}(?:\.[0-9]{1,10})?$'
+
+    if (not re.match(reg_exp, str(longitude)) or not re.match(reg_exp, str(longitude))):
+        print("entered data is not correct")
+
+    closest_distance = None
+    bar = None
+    for item in data['features']:
+        item_longitude = item['geometry']['coordinates'][0]
+        item_latitude = item['geometry']['coordinates'][1]
+
+        distance = sqrt((item_longitude - longitude)**2 + (item_latitude - latitude)**2);
+
+        if closest_distance == None or closest_distance > distance:
+            bar = item
+            closest_distance = distance
+
+    return bar['properties']['Attributes']['Name'] if not (bar is None) else "failed"
 
 if __name__ == '__main__':
-    pass
+
+    filepath = "bars.json"
+    deserializes_json = load_data(filepath)
+    if (deserializes_json):
+
+        print ("самый большой бар: ", get_biggest_bar(deserializes_json))
+        print ("самый маленький бар: ", get_smallest_bar(deserializes_json))
+
+        longitude = float(input('Enter longitude:'))
+        latitude = float(input('Enter latitude:'))
+        print("самый близкий бар: ", get_closest_bar(deserializes_json, longitude, latitude))
